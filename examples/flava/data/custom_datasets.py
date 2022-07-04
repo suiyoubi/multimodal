69b4e0cf2ee7356c195f96633b2710a1d39743dd
aot <aot@nvidia.com>
2022-07-04 08:24:03 -0700

    dataset load description/title. replace '+' with ' '

--- examples/flava/data/custom_datasets.py
+++ examples/flava/data/custom_datasets.py
@@ -20,7 +20,10 @@ class YFCCDataset(Dataset):
         image_key = self.df.iloc[idx, 0]
         image_filename = os.path.join(self.image_root, image_key[:3], image_key[3:6], image_key) + self.ext
         image = Image.open(image_filename)
-        text = self.df.iloc[idx, 1]
+        # Use description field if exists. Otherwise use title field instead.
+        # Replace seprator from "+" with " "
+        text = self.df.iloc[idx, 2] if self.df.iloc[idx, 2] is not None else self.df.iloc[idx, 1]
+        text = text.replace('+', ' ')
         # Transforms
         output = {}
         if self.itm_probability > 0:
@@ -45,3 +48,17 @@ class YFCCDataset(Dataset):
             text_infos[info] = text_infos[info].squeeze()
         output.update(text_infos)
         return output
+
+if __name__ == '__main__':
+    from .transforms import (
+    default_text_transform,
+    TEXT_DEFAULT_TOKENIZER,
+    VL_MAX_LENGTH_DEFAULT,
+    )
+    from transformers import BertTokenizerFast
+    text_tokenizer = BertTokenizerFast.from_pretrained(TEXT_DEFAULT_TOKENIZER) # should use BertTokenizerFast
+    text_transform = default_text_transform(text_tokenizer, max_text_length=VL_MAX_LENGTH_DEFAULT)
+    text = 'This+is+a+sample+sentence.'
+    print(f'+ separator: {text_transform(text)}')
+    text = 'This is a sample sentence.'
+    print(f'space separator: {text_transform(text)}')
